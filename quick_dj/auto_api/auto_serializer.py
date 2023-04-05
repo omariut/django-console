c081ce2e3e33c6d1ba1d879001c643459b14390c
omariut <omarcpgcbl@gmail.com>
2023-04-05 16:50:44 +0600

    autoview & auto-serializer

--- quick_dj/auto_api/auto_serializer.py
+++ quick_dj/auto_api/auto_serializer.py
@@ -1,24 +1,6 @@
 
 import os
 
-def get_serializer_header(model_name):
-    return f"class {model_name}Serializer(serializers.ModelSerializer):\n"
-
-def get_serializer_body(model_name):
-    return f"\tmodel = {model_name}\n\tfields = '__all__'"
-
-def get_serializer_string(model_name):
-    serializer_header=get_serializer_header(model_name)
-    serializer_body=get_serializer_body(model_name)
-    return serializer_header + serializer_body + "\n"
-
-def write_serializer(model_name,file):
-    file="product/serializers.py"
-    model_name="Product"
-    serializer_string=get_serializer_string(model_name)
-    with open(file, 'a') as f:
-        f.write(serializer_string)
-
 class ModelSerializerWriter:
     def __init__(self, app_name,model_name):
         self.model_name = model_name
